1923803e6dbb399ebe3681033aa0475177617b62
christopher <stopher.kirkum@gmail.com>
2021-04-21 15:32:18 -0700

    thirdAnagram almost done

--- 02-anagrams.js
+++ 02-anagrams.js
@@ -38,9 +38,23 @@ function secondAnagram(str1, str2) {
 // console.log(secondAnagram("elvis", "lives"));
 
 function thirdAnagram(str1, str2) {
-  // Code goes here ....
+
+  let arr1 = str1.split("");
+  let arr2 = str2.split("");
+  let obj1 = {};
+  let obj2 = {};
+
+  for(let i = 0; i < arr1.length; i++){
+    obj1[arr1[i]] += 1
+    obj2[arr2[i]] += 1
+  }
+  console.log(obj1)
+  console.log(obj2)
+
 }
 
+console.log(thirdAnagram("gizmo", "sally"))
+console.log(thirdAnagram("elvis", "lives"))
 
 function fourthAnagram(str1, str2) {
   // Code goes here ....
